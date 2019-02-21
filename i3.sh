c0921bd351347cb0d7b12f6d4043bdc3d497ad45
Dimitar Apostolov <d.apostolov@gmail.com>
2019-02-21 09:49:24 +0100

    Update polybar and compton configs

--- i3.sh
+++ i3.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-if (! (which i3blocks > /dev/null)); then
-  echo "Installing i3blocks ..."
-  sudo apt install i3blocks
+# Install polybar
+if (! (which polybar > /dev/null)); then
+  echo "Polybar neesds to be insalled"
 fi
 
 ln -s ~/.dotfiles/i3 ~/.config/i3
-ln -s ~/.dotfiles/i3/i3blocks.conf ~/.i3blocks.conf
-
