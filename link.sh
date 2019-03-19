6f5c76319299304f5f2e73b050cab7c3b8be5107
Dimitar Apostolov <d.apostolov@gmail.com>
2019-03-19 10:52:28 +0100

    Link ctags and git templates

--- link.sh
+++ link.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
-#Git
+# Create a backup folder where all existing dotfiles will be saved to
 mkdir -p ~/.dotfiles_bkp
 
+#Git
 mv ~/.gitconfig ~/.dotfiles_bkp/.gitconfig
-ln -sf ~/.dotfiles/copy/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/copy/gitconfig ~/.gitconfig
 
 mv ~/.gitignore ~/.dotfiles_bkp/.gitignore
-ln -sf ~/.dotfiles/copy/gitignore ~/.gitignore
+ln -s ~/.dotfiles/copy/gitignore ~/.gitignore
 
-ln -sf ~/.dotfiles/copy/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.dotfiles/copy/git-prompt.sh ~/.git-prompt.sh
+
+ln -s ~/.dotfiles/copy/git_templates ~/.git_templates
 
 # Tmux
 mv ~/.tmux.conf ~/.dotfiles_bkp/.tmux.conf
-ln -sf ~/.dotfiles/copy/tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/copy/tmux ~/.tmux
+ln -s ~/.dotfiles/copy/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/copy/tmux ~/.tmux
 
 
 # Neovim
 mv ~/.vimrc ~/.dotfiles_bkp/.vimrc
-ln -sf ~/.dotfiles/copy/vimrc ~/.vimrc
+ln -s ~/.dotfiles/copy/vimrc ~/.vimrc
 
+# ctags
+ln -s ~/.dotfiles/copy/ctags.d ~/.ctags.d
 
 # Bash
 echo "source ~/.dotfiles/bashrc" >> ~/.bashrc
