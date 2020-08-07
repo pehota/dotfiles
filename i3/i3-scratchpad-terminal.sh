e1f87ff3f91fe867930e0b03eee4e5c0d187f27a
Dimitar Apostolov <d.apostolov@gmail.com>
2020-08-07 15:44:33 +0200

    Add Tools mode to i3

--- i3/i3-scratchpad-terminal.sh
+++ i3/i3-scratchpad-terminal.sh
@@ -2,12 +2,18 @@
 # Start a terminal (if there is none), and show (toggle) it as i3 scratchpad
 # See <https://www.reddit.com/r/i3wm/comments/8q44zr/script_to_runs_scratchpad_if_not_running/>
 
-TITLE="_scratchpad"
+APP="$1"
+TITLE="::scratchpad_terminal::"
+COMMAND=""
+
+if command -v "$APP" > /dev/null 2>&1; then
+	COMMAND="bash -c $APP"
+	TITLE="::${APP}::"
+fi
 
 if ! pgrep -f "$TITLE"; then
-	# i3-msg "exec --no-startup-id i3-sensible-terminal --class '$TITLE'"
-	i3-msg "exec --no-startup-id st -c '$TITLE' -f 'Source Code Pro:size=12'"
+	i3-msg "exec --no-startup-id i3-sensible-terminal --class '$TITLE' $COMMAND"
 	sleep 0.5
 fi
 
-i3-msg "[class=\"$TITLE\"] scratchpad show; sticky enable"
+i3-msg "[class=\"$TITLE\"] scratchpad show; sticky enable floating enable"
