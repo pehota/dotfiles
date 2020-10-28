84f707f952a8cd04385ceee070b38ed16724d38d
Dimitar Apostolov <d.apostolov@gmail.com>
2020-10-28 22:49:19 +0100

    Clean cmus files

--- cmus/lib.pl
+++ cmus/lib.pl
@@ -1,4 +1,5 @@
 http://stream.bnr.bg:8011/horizont.aac.m3u
+http://coderadio-relay.freecodecamp.org/radio/8010/radio.mp3
 http://stream.radioreklama.bg:80/radio1.aac
 http://stream.radioreklama.bg:80/radio1rock.aac
 http://s10.voscast.com:9878/;
