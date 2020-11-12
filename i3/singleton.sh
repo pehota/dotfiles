d89e1be31b5c6d6b13c47cc1f60fba790da2200e
Dimitar Apostolov <d.apostolov@gmail.com>
2020-11-12 15:17:45 +0100

    WIP: Latest changes

--- i3/singleton.sh
+++ i3/singleton.sh
@@ -2,37 +2,42 @@
 # Start a terminal (if there is none), and show (toggle) it as i3 scratchpad
 # See <https://www.reddit.com/r/i3wm/comments/8q44zr/script_to_runs_scratchpad_if_not_running/>
 
-TITLE="__${1:-i3-sensible-terminal}__"
-WORKSPACE="${2}"
+TERMINAL_APP="i3-sensible-terminal"
+APP="${1:-$TERMINAL_APP}"
+TITLE="__${APP}__"
+WORKSPACE="${2:-scratchpad}"
 I3_MSG="focus"
 
-if ! pgrep -f "$TITLE"; then
+# if ! pgrep -f "$APP"; then
+if test ! $(pidof -s "$APP"); then
 	COMMAND=""
 
-	if command -v "$1" > /dev/null 2>&1; then
-		COMMAND="bash -c $1"
-	fi
-
-	if [[ -z $COMMAND && $TITLE != "__i3-sensible-terminal__" ]]; then
+	if "$APP" == "$TERMINAL_APP"; then
+		COMMAND=""
+	elif (command -v "$APP" > /dev/null 2>&1); then
+		COMMAND="bash -c $APP"
+	else
 		exit 0
 	fi
 
+	echo "exec --no-startup-id $TERMINAL_APP --class '$TITLE' $COMMAND"
+
 	case "$WORKSPACE" in
 		scratchpad)
-			I3_MSG="scratchpad show sticky enable floating enable; focus"
+			I3_MSG="scratchpad show sticky enable floating enable, focus"
 			;;
 		"")
 			I3_MSG="focus"
 			;;
 		*)
-			I3_MSG="focus, move container to workspace ${WORKSPACE},workspace ${WORKSPACE}"
+			I3_MSG="focus;move container to workspace ${WORKSPACE};workspace ${WORKSPACE}"
 	esac
 
-	i3-msg "exec --no-startup-id i3-sensible-terminal --class '$TITLE' $COMMAND"
+	i3-msg "exec --no-startup-id $TERMINAL_APP --class '$TITLE' $COMMAND"
 
 	sleep 0.5
 fi
 
-echo "MSG: $I3_MSG; $WORKSPACE"
+echo "MSG: $I3_MSG; $WORKSPACE; $APP"
 
 i3-msg "[class=\"$TITLE\"] $I3_MSG"
