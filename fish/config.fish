f6c34d14ae620feebf1f95bf336005507a27139c
Dimitar Apostolov <d.apostolov@gmail.com>
2021-05-17 16:46:03 +0200

    Update bat theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -e EDITOR; set -Ux EDITOR (command -v nvim)
 set -e VISUAL; set -Ux VISUAL $EDITOR
-set -e BAT_THEME; set -Ux BAT_THEME gruvbox
+set -e BAT_THEME; set -Ux BAT_THEME gruvbox-dark
 
 if status is-interactive
   # Key bindings: vim and default emacs
