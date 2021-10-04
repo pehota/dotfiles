dd0798dfdf2c7fc66bca3af8433c8da9778be28d
Dimitar Apostolov <d.apostolov@gmail.com>
2021-10-05 00:01:33 +0200

    Use correct stow dir

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -14,12 +14,16 @@ for package in $PACKAGES_FOLDER/*/; do
   package_name=$(basename $package)
   stow_dir="$STOW_DIR"
   stow_target="$STOW_TARGET"
+  stow_package="$package_name"
+
+  if [[ -f "$package/pre-install" || -f "$package/post-install" ]]; then
+    stow_dir="$package"
+    stow_package="content"
+  fi
 
   if (! (isPackageInstalled $package_name)); then
     if [[ -f "$package/pre-install" ]]; then
       exec "$package/pre-install"
-      stow_dir="$package"
-      package_name="content"
     fi
 
     echo "Installing $package_name ..."
@@ -27,12 +31,11 @@ for package in $PACKAGES_FOLDER/*/; do
 
     if [[ -f "$package/post-install" ]]; then
       exec "$package/post-install"
-      stow_dir="$package"
-      package_name="content"
     fi
     echo "done"
   else
     echo "$package_name already installed"
   fi
-  stow -d "$stow_dir" -t "$stow_target" "$package_name"
+  echo "stow -d "$stow_dir" -t "$stow_target" "$stow_package""
+  stow -d "$stow_dir" -t "$stow_target" "$stow_package"
 done
