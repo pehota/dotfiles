fd7c358a65c8dea20a0a2ffb83f1ddb4dffca765
Dimitar Apostolov <d.apostolov@gmail.com>
2022-02-15 21:50:41 +0100

    Clean up scripts

--- scripts/setup-ranger.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ! -d ~/.config/ranger/plugins/ranger_devicons ]]; then
-  echo "  Installing devicons for ranger ..."
-  git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-fi
