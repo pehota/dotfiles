bafbbb732a8d3663016b879d847ea7f21cd023ec
Dimitar Apostolov <d.apostolov@gmail.com>
2022-02-16 09:32:37 +0100

    Add installation for AppCleaner

--- scripts/install-utils.sh
+++ scripts/install-utils.sh
@@ -11,5 +11,6 @@ declare -A packages=(
   ["exa"]="exa" # Improved ls
   ["python3"]="python3" # needed for neovim
   ["rg"]="ripgrep" # fast file search
+  ["appcleaner"]="appcleaner" # Uninstall apps not installed with brew
 )
 installPackages packages
