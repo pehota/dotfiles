d547fbe8ca37c889a81da64827e6d872383472a1
Dimitar Apostolov <d.apostolov@gmail.com>
2023-03-20 14:57:10 +0100

    Update scripts formatting

--- scripts/install-utils.sh
+++ scripts/install-utils.sh
@@ -6,17 +6,16 @@ source "$SCRIPTS_FOLDER/utils.sh"
 
 # Install common packages
 declare -A packages=(
-  ["bat"]="bat" # Improved cat
-  ["curl"]="curl"
-  ["exa"]="exa" # Improved ls
-  ["python3"]="python3" # needed for neovim
-  ["rg"]="ripgrep" # fast file search
-  ["appcleaner"]="appcleaner" # Uninstall apps not installed with brew
-  ["direnv"]="direnv" # folder specific shel environment
-  ["fzf"]="fzf" # fast file search
+	["bat"]="bat" # Improved cat
+	["curl"]="curl"
+	["exa"]="exa"               # Improved ls
+	["python3"]="python3"       # needed for neovim
+	["rg"]="ripgrep"            # fast file search
+	["appcleaner"]="appcleaner" # Uninstall apps not installed with brew
+	["direnv"]="direnv"         # folder specific shel environment
+	["fzf"]="fzf"               # fast file search
 )
 installPackages packages
-
 
 installPackage "homebrew/cask-fonts/font-fira-code-nerd-font"
 installPackage "homebrew/cask-fonts/font-jetbrains-mono-nerd-font"
