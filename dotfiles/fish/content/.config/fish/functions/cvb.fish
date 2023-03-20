aa6f376c9d71c1a7ed3d56cf0693b08e76fcf335
Dimitar Apostolov <d.apostolov@gmail.com>
2023-03-20 14:53:18 +0100

    Update function to clean up nvim swaps

--- dotfiles/fish/content/.config/fish/functions/cvb.fish
+++ dotfiles/fish/content/.config/fish/functions/cvb.fish
@@ -1,4 +1,4 @@
 function cvb --description 'Clean Vim backup files'
-	rm ~/.vim/backup/*.* 2>/dev/null
 	rm ~/.local/share/nvim/swap/*.* 2>/dev/null
+	rm ~/.local/state/nvim/swap/*.* 2>/dev/null
 end
