387e616108ec6a8abdb52aaef3e56831d6bcd203
Dimitar Apostolov <dimitar.apostolov@climatepartner.com>
2023-07-28 10:13:16 +0200

    Update function for auto-switching node version

--- dotfiles/fish/content/.config/fish/functions/__switch_node_version.fish
+++ dotfiles/fish/content/.config/fish/functions/__switch_node_version.fish
@@ -1,5 +1,10 @@
-function __switch_node_version --on-variable FNM_NODE_VERSION
-  if test "$FNM_NODE_VERSION" != ""; and test (type -t fnm) = "file"
-    fnm use "$FNM_NODE_VERSION"
+function __switch_node_version --on-variable FNM_NODE_VERSION --on-variable PWD
+  if test (type -t fnm) != "file"
+    return
+  end
+  if test "$FNM_NODE_VERSION" != ""
+    fnm use "$FNM_NODE_VERSION" --log-level=quiet
+  else
+    fnm use default --log-level=quiet
   end
 end
