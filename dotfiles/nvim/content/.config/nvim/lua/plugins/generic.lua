e69bd0275dbb431f09cfe3d739d30ab0429c4e50
Dimitar Apostolov <d.apostolov@gmail.com>
2023-10-01 14:48:43 +0200

    Update nvim config
    Switch null-ls to nvimtools/none-ls.

--- dotfiles/nvim/content/.config/nvim/lua/plugins/generic.lua
+++ dotfiles/nvim/content/.config/nvim/lua/plugins/generic.lua
@@ -49,7 +49,7 @@ return {
 			},
 		},
 		{
-			"jose-elias-alvarez/null-ls.nvim",
+			"nvimtools/none-ls.nvim",
 			opts = function()
 				local null_ls = require("null-ls")
 				require("lspconfig").tsserver.setup({
