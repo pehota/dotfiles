0297cfb059b8ed48b8e831ac26475a1a1b45935a
Dimitar Apostolov <d.apostolov@gmail.com>
2023-10-02 11:45:45 +0200

    Update nvim dependencies installation and config

--- dotfiles/nvim/content/.config/nvim/lua/config/options.lua
+++ dotfiles/nvim/content/.config/nvim/lua/config/options.lua
@@ -8,3 +8,7 @@ vim.o.clipboard = "unnamedplus"
 vim.o.splitright = true
 vim.o.splitbottom = true
 vim.o.tabstop = 2
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
