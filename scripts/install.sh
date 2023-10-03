0a87ceeb2e4688c8dcdee2bdbc63c95cb4070769
Dimitar Apostolov <d.apostolov@gmail.com>
2023-10-03 11:51:39 +0200

    Update the runtime check for install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,20 +4,7 @@ SCRIPTS_FOLDER=$(dirname "$0")
 
 source "$SCRIPTS_FOLDER/utils.sh"
 
-if [[ ! checkOs ]]; then
-  echo "Unsupported OS"
-  exit 1
-fi
-
-if [[ -z "$BASH_VERSINFO" ]]; then
-  echo "Please run the script in bash"
-  exit 0
-fi
-
-if [[ "${BASH_VERSINFO:-0}" -le 3 ]]; then
-  echo "Bash version is too old. Please upgrade to v4 or higher."
-  exit 1
-fi
+checkRuntime
 
 bootstrap
 
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -11,82 +11,102 @@ BACKUP_DIR=~/.dotfiles/.backup
 mkdir -p $BACKUP_DIR
 
 checkOs() {
-  if [[ "$(uname)" == "Darwin" ]]; then
-    return 0
-  else
-    return 1
-  fi
+	if [[ "$(uname)" == "Darwin" ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+checkRuntime() {
+	if [[ ! checkOs ]]; then
+		echo "Unsupported OS"
+		exit 1
+	fi
+
+	if [[ -z "$BASH_VERSINFO" ]]; then
+		echo "Please run the script in bash"
+		exit 0
+	fi
+
+	local BASH_VERSION_PARSED
+
+	BASH_VERSION_PARSED=$(echo "$BASH_VERSION" | grep -Eo '[0-9]+\.[0-9]+')
+
+	if [[ "${BASH_VERSION_PARSED:-0}" -le 3 ]]; then
+		echo "Bash version is too old. Please upgrade to v4 or higher."
+		exit 1
+	fi
 }
 
 installPackageManager() {
-  # Install brew if needed
-  if (!(isPackageInstalled "brew")); then
-    echo "Installing brew ..."
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  fi
+	# Install brew if needed
+	if (!(isPackageInstalled "brew")); then
+		echo "Installing brew ..."
+		bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
 }
 
 bootstrap() {
-  installPackageManager
+	installPackageManager
 
-  if (!(isPackageInstalled "stow")); then
-    installPackage "stow"
-  fi
+	if (!(isPackageInstalled "stow")); then
+		installPackage "stow"
+	fi
 }
 
 isPackageInstalled() {
-  test $(type -t "$1") || brew list -q -1 "$1" 2> /dev/null
+	test $(type -t "$1") || brew list -q -1 "$1" 2>/dev/null
 }
 
 installPackage() {
-  brew install "$1"
+	brew install "$1"
 }
 
 installPackages() {
-  local -n packages_ref=$1
-  local packages_to_install=""
+	local -n packages_ref=$1
+	local packages_to_install=""
 
-  for c in "${!packages_ref[@]}"
-  do
-    if (! (isPackageInstalled "$c")); then
-      if [[ -n "$packages_to_install" ]]; then
-        packages_to_install="$packages_to_install ${packages_ref[$c]}"
-      else
-        packages_to_install="${packages_ref[$c]}"
-      fi
-    fi
-  done
+	for c in "${!packages_ref[@]}"; do
+		if ( ! (isPackageInstalled "$c")); then
+			if [[ -n "$packages_to_install" ]]; then
+				packages_to_install="$packages_to_install ${packages_ref[$c]}"
+			else
+				packages_to_install="${packages_ref[$c]}"
+			fi
+		fi
+	done
 
-  if [[ -n "$packages_to_install" ]]; then
-    installPackage $packages_to_install
-  fi
+	if [[ -n "$packages_to_install" ]]; then
+		installPackage $packages_to_install
+	fi
 }
 
 createSimlink() {
-  # $source should be either a full path or a relative path under ~/.dotfiles
-  local source="$1"
-  local target="$2"
+	# $source should be either a full path or a relative path under ~/.dotfiles
+	local source="$1"
+	local target="$2"
 
-  # If no target is provided - use the name of the source
-  if [[ -z "$target" ]]; then
-    target="~/.$source"
-  fi
+	# If no target is provided - use the name of the source
+	if [[ -z "$target" ]]; then
+		target="~/.$source"
+	fi
 
-  if [[ $source != *"$WORKING_DIR"* ]]; then
-    source="${WORKING_DIR}/${source}"
-  fi
+	if [[ $source != *"$WORKING_DIR"* ]]; then
+		source="${WORKING_DIR}/${source}"
+	fi
 
-  if [[  ! -L "$target"  && -e "$source" ]]; then
-    local target_dir=$(dirname $target)
+	if [[ ! -L "$target" && -e "$source" ]]; then
+		local target_dir=$(dirname $target)
 
-    if [[ -e "$target" && -f "$target" ]]; then
-      mv $target $BACKUP_DIR &> /dev/null
-    fi
+		if [[ -e "$target" && -f "$target" ]]; then
+			mv $target $BACKUP_DIR &>/dev/null
+		fi
 
-    if [[ ! -d "$target_dir" ]]; then
-      mkdir -p $target_dir
-    fi
+		if [[ ! -d "$target_dir" ]]; then
+			mkdir -p $target_dir
+		fi
 
-    ln -sf $source $target
-  fi
+		ln -sf $source $target
+	fi
 }
