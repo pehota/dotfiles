92cad373086ef98ce6f1e4157482fedf475ce041
Dimitar Apostolov <dimitar.apostolov@climatepartner.com>
2023-10-04 09:27:24 +0200

    Update nvim plugins config

--- dotfiles/nvim/content/.config/nvim/lua/plugins/disabled.lua
+++ dotfiles/nvim/content/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,8 @@
 -- disable trouble
 return {
-  { "ggandor/leap.nvim", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
-  { "echasnovski/mini.animate", enabled = false },
+	{ "ggandor/leap.nvim", enabled = false },
+	{ "ggandor/flit.nvim", enabled = false },
+	{ "echasnovski/mini.animate", enabled = false },
+	{ "folke/flash.nvim", enabled = false },
+	{ "rcarriga/nvim-notify", enabled = false },
 }
