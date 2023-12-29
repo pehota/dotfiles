38e8a89f6413924fa2698337a232fdc9d8b8a53b
Dimitar Apostolov <>
2023-12-29 16:39:24 +0200

    Add LazyExtras config

---
+++ dotfiles/nvim/content/.config/nvim/lua/plugins/lazy-extras.lua
@@ -0,0 +1,9 @@
+return {
+	{ import = "lazyvim.plugins.extras.lang.docker" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.terraform" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.test.core" },
+}
