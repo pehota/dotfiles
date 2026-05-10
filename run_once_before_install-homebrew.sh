7327cadf46ed07d7fc084b27d1b8139c475aa20e
Dimitar Apostolov <d.apostolov@gmail.com>
2026-05-10 13:14:58 +0200

    fix: don't install brew on linux

--- run_once_before_install-homebrew.sh
+++ run_once_before_install-homebrew.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+[ "$(uname)" = "Darwin" ] || exit 0
 command -v brew >/dev/null 2>&1 && exit 0
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
