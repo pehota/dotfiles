9ed57862c798d64ad108efc24eae2c45d02319b0
Dimitar Apostolov <d.apostolov+mac@gmail.com>
2026-09-10 12:56:25 +0200

    refactor: switch node version works with direct calls

--- dot_config/fish/functions/__switch_node_version.fish
+++ dot_config/fish/functions/__switch_node_version.fish
@@ -2,8 +2,12 @@ function __switch_node_version --on-variable FNM_NODE_VERSION --on-variable PWD
   if test (type -t fnm) != "file"
     return
   end
-  if test "$FNM_NODE_VERSION" != ""
-    fnm use "$FNM_NODE_VERSION" --log-level=quiet
+  set -l node_version "$FNM_NODE_VERSION"
+  if test -z "$node_version" -a -f .envrc
+    set node_version (bash -c 'source .envrc 2>/dev/null && echo $FNM_NODE_VERSION')
+  end
+  if test -n "$node_version"
+    fnm use "$node_version" --log-level=quiet
   else
     fnm use default --log-level=quiet
   end
